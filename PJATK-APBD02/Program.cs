67695cb659b3d981904a7daaac5faa7dcb231f21
Kacper <kacper.piotrowski1711@gmail.com>
2026-03-18 18:53:32 +0100

    Added input handling

--- PJATK-APBD02/Program.cs
+++ PJATK-APBD02/Program.cs
@@ -4,7 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello, APBD Lab2!");
+        double[] numbers = new double[5];
+        Console.WriteLine("You will enter five numbers!");
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine("{0}.Enter number: ", i+1);
+            numbers[i] = double.Parse(Console.ReadLine());
+        }
     }
 }
 
